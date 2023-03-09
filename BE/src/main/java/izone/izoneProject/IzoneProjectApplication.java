9014d444f37bdc0748fecf5f4cca0c721bed5ff6
snowqhdl <snowqhdl@gmail.com>
2023-03-09 14:57:46 +0900

    [init]레포지토리수정

---
+++ BE/src/main/java/izone/izoneProject/IzoneProjectApplication.java
@@ -0,0 +1,13 @@
+package izone.izoneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IzoneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IzoneProjectApplication.class, args);
+	}
+
+}
