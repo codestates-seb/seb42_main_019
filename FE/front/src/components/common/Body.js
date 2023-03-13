3d0a50d6434134f83381a5fe0985b75fb7c87d8a
journey0321 <yjyjh0321@gmail.com>
2023-03-13 15:03:56 +0900

    [design]app.js

--- FE/front/src/components/common/Body.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "../common/Body.module.css";
-
-const Main = ({ children }) => {
-	return <div className={styles.Contents}>{children}</div>;
-};
-
-export default Main;
