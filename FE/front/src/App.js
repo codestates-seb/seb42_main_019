cbe94bd3cfcd62e40269f199e6764aa7edd5caca
ASOpaper <hiji0907@gmail.com>
2023-03-13 16:36:32 +0900

    [feat]테스트용 라우터

--- FE/front/src/App.js
+++ FE/front/src/App.js
@@ -1,16 +1,29 @@
 import style from './App.module.css';
 import Nav from './components/common/Nav';
-import Header2 from './components/common/Header2';
+import Home from './pages/SB/Home'
+import Alert from './pages/SB/Alert'
+import Bookplus from './pages/JH/CreateBook'
+import Booklist from './pages/SB/MyBookShelf'
+import Mypage from './pages/SB/Mypage'
 import Background from '../src/components/common/Background';
 import Login from '../src/pages/JH/Login';
+import BookSearch from '../src/pages/HJ/BookSearch';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className={style.App}>
 			<Background></Background>
 			<div className={style.body}>
-				<Header2 />
-				<Login />
+				<Routes>
+					<Route path='/' element={<Home />}></Route>
+					<Route path='/alert' element={<Alert />}></Route>
+					<Route path='/bookplus' element={<Bookplus/>}></Route>
+					<Route path='/booklist' element={<Booklist />}></Route>
+					<Route path='/mypage' element={<Mypage />}></Route>
+					<Route path='/login' element={<Login />}></Route>
+					<Route path='/booksearch' element={<BookSearch />}></Route>
+				</Routes>
 				<Nav />
 			</div>
 		</div>
