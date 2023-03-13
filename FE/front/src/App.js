61e564459ecd9c139aaa964099478185ca54f77d
ASOpaper <hiji0907@gmail.com>
2023-03-13 14:25:49 +0900

    [feat]네비게이션

--- FE/front/src/App.js
+++ FE/front/src/App.js
@@ -1,10 +1,12 @@
 import style from './App.module.css';
 import Home from './pages/SB/Home';
+import Nav from './components/common/Nav'
 
 function App() {
 	return (
 		<div className={style.App}>
 			<Home />
+			<Nav />
 		</div>
 	);
 }
---
+++ FE/front/src/components/common/Nav.js
@@ -0,0 +1,55 @@
+import styles from './Nav.module.css';
+import classNames from 'classnames/bind';
+import { ReactComponent as IconHome } from '../../assets/IconHome.svg';
+import { ReactComponent as IconAlert } from '../../assets/IconAlert.svg';
+import { ReactComponent as IconBookplus } from '../../assets/Bookplus.svg';
+import { ReactComponent as IconBooklist } from '../../assets/IconBooklist.svg';
+import { ReactComponent as IconMypage } from '../../assets/IconMypage.svg';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+function Navigation() {
+  const cx = classNames.bind(styles);
+  const [currentPage, setCurrentPage] = useState(1);
+  console.log(currentPage);
+  return (
+    <>
+      <nav>
+        <ul className={cx('nav')}>
+          <li className={cx('nav--li')}>
+            <Link to='/' onClick={() => setCurrentPage(1)}>
+              <IconHome fill={currentPage === 1 ? '#2F5A2D' : '#D9D9D9'} />
+              <span>홈</span>
+            </Link>
+          </li>
+          <li className={cx('nav--li')}>
+            <Link to='/' onClick={() => setCurrentPage(2)}>
+              <IconAlert fill={currentPage === 2 ? '#2F5A2D' : '#D9D9D9'} />
+              <span>알림</span>
+            </Link>
+          </li>
+          <li className={cx('nav--li')}>
+            <Link to='/' onClick={() => setCurrentPage(3)}>
+              <IconBookplus fill={currentPage === 3 ? '#2F5A2D' : '#D9D9D9'} />
+              <span>책 등록</span>
+            </Link>
+          </li>
+          <li className={cx('nav--li')}>
+            <Link to='/' onClick={() => setCurrentPage(4)}>
+              <IconBooklist fill={currentPage === 4 ? '#2F5A2D' : '#D9D9D9'} />
+              <span>내 책장</span>
+            </Link>
+          </li>
+          <li className={cx('nav--li')}>
+            <Link to='/' onClick={() => setCurrentPage(5)}>
+              <IconMypage fill={currentPage === 5 ? '#2F5A2D' : '#D9D9D9'} />
+              <span>마이페이지</span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Navigation;
