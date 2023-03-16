0667c87b1d4fffde2540718ffa093113e3d9cee4
journey0321 <yjyjh0321@gmail.com>
2023-03-16 09:44:26 +0900

    [design]회원가입,구매자판매자 상세페이지

--- FE/front/src/pages/JH/SellerDetailViewEdit.js
+++ FE/front/src/pages/JH/SellerDetailViewEdit.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import styles from './CustomerDetailView.module.css';
+import Header2 from '../../components/common/Header2';
+import Button from '../../components/common/Button';
 
 const SellerDetailViewEdit = () => {
-	return <div></div>;
+	return (
+		<div>
+			<div className={styles.Main}>
+				<Header2>등록된 책</Header2>
+				<div className={styles.Text}>
+					<label>책 설명</label>
+					<div className={styles.TextIner}>
+						또 읽으려고 가지고 있었는데, 다른책이 더 읽고 싶어졌어요.
+					</div>
+				</div>
+				<div className={styles.BookState}>
+					<label>책 상태</label>
+					<div className={styles.State}>상</div>
+				</div>
+				<Button>수정하기</Button>
+			</div>
+		</div>
+	);
 };
 
 export default SellerDetailViewEdit;
--- FE/front/src/pages/JH/SignUp.js
+++ FE/front/src/pages/JH/SignUp.js
@@ -11,30 +11,30 @@ const SignUp = () => {
 	return (
 		<main className={styles.Main}>
 			<Header2>회원가입</Header2>
+			<label>이메일 주소</label>
 			<div className={styles.Email}>
-				<label>이메일 주소</label>
 				<input type='text' placeholder='example@gamil.com' />
 			</div>
+			<label>비밀번호</label>
 			<div className={styles.PassWord}>
-				<label>비밀번호</label>
 				<input
 					type='password'
 					placeholder='영문,숫자,특수문자 조합 6~20자'
 				></input>
 			</div>
+			<label>닉네임</label>
 			<div className={styles.NickName}>
-				<label>닉네임</label>
 				<input type='text' placeholder='한글 또는 영문 6~12자' />
 				<button>
 					<CheckIcon />
 				</button>
 			</div>
+			<label>사는 곳</label>
 			<div className={styles.City}>
-				<label>사는 곳</label>
 				<div className={styles.Value}>
 					<span>시 선택</span>
 					<button onClick={(e) => setDropdownVisibility(!dropdownVisibility)}>
-						{dropdownVisibility ? <DownIcon /> : <UpIcon />}
+						{dropdownVisibility ? <UpIcon /> : <DownIcon />}
 					</button>
 				</div>
 			</div>
