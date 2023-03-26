cf59496863a34a2969f05549a6688eeb826dee68
ASOpaper <hiji0907@gmail.com>
2023-03-27 05:25:45 +0900

    [feat]검색기능 반쪽의 성공

--- FE/front/src/components/JSB/Search.js
+++ FE/front/src/components/JSB/Search.js
@@ -2,14 +2,15 @@ import style from './Search.module.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const HomeSearch = function () {
+const HomeSearch = function ( { currentQuery, setCurrentQuery, searchBook } ) {
 
-	const [searchQuery, setSearchQuery] = useState('');
+	// const [searchQuery, setSearchQuery] = useState('');
   const navigate = useNavigate()
 
 	const handleSearch = (event) => {
 		if (event.key === 'Enter') {
-		  navigate(`/search?q=${searchQuery}`);
+		  navigate(`/search?q=${currentQuery}`);
+      searchBook()
 		}
 	}
 
@@ -22,7 +23,7 @@ const HomeSearch = function () {
           placeholder="지금 당신이 읽고 싶은 책은 바로!..."
           onChange={(e)=>
             {
-              setSearchQuery(e.target.value)
+              setCurrentQuery(e.target.value)
               e.preventDefault();
             }
           }
--- FE/front/src/components/KHJ/BS.js
+++ FE/front/src/components/KHJ/BS.js
@@ -1,20 +1,28 @@
+import classNames from 'classnames/bind';
 import style from './BSlist.module.css';
 
 const BS = function ({ bookData }) {
+	const cx = classNames.bind(style)
+	const authors = bookData.authors.join(', ');
+	const userOn = () => {
+		if(bookData.user) return true;
+		return false;
+	}
+
 	return (
 		<>
-        <div className={style.box1}>
-				<div className={style.notFooter}>
-					<div className={style.listboxMessage}>
-						<div className={style.bookimg1}>
-							<img key={bookData.id} className={style.img1} src={bookData.thumbnail} alt='bookcover' />
+        	<div className={cx('box1')}>
+				<div className={cx('notFooter')}>
+					<div className={cx('listboxMessage')}>
+						<div className={cx('bookimg1')}>
+							<img key={bookData.id} className={cx('img1')} src={bookData.thumbnail} alt='bookcover' />
 						</div>
-						<div className={style.bookTitle}>
-							<p className={style.bookTitlep}>{bookData.title}</p>
+						<div className={cx('bookTitle')}>
+							<p className={cx('bookTitlep')}><b>{bookData.title}</b></p>
 						</div>
-						<div className={style.bookDetail1}>
-							<p className={style.bookDetail01}>{bookData.name}({bookData.region})</p>
-							<p className={`${style.bookDetail01} ${style.bookDetail02}`}>
+						<div className={cx('bookDetail1')}>
+							<p className={cx('bookDetail01')}>{bookData.name}{userOn() ? `${bookData.user.name}(${bookData.user.region})` : authors}</p>
+							<p className={cx('bookDetail01', 'bookDetail02')}>
 								{bookData.time}시간 전
 							</p>
 						</div>
