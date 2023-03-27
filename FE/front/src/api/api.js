9e2e3673d981cdffc759cddf84b1483a7eb7284e
ASOpaper <102563367+ASOpaper@users.noreply.github.com>
2023-03-28 02:13:14 +0900

    Update api.js

--- FE/front/src/api/api.js
+++ FE/front/src/api/api.js
@@ -4,7 +4,6 @@ const api = axios.create({
 	baseURL: 'http://ec2-15-164-49-209.ap-northeast-2.compute.amazonaws.com:8080',
 	headers : {
 		withCredentials : true,
-		'X-Requested-With': 'XMLHttpRequest',
 	}
 });
 
