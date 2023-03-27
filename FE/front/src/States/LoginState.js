1b0b1c7732a93f857acfab33b27944db1437d60e
lunadein2022 <lunadein2022@gmail.com>
2023-03-27 10:28:57 +0900

    [test]file

---
+++ FE/front/src/States/LoginState.js
@@ -0,0 +1,55 @@
+import { atom, selector, useRecoilState } from 'recoil';
+import axios from 'axios';
+
+const authState = atom({
+    key: 'authState',
+    default: {
+        password: '',
+        email: '',
+        name: '',
+        region: '',
+        commentCount: '',
+        likeCount: '',
+        disLikeCount: '',
+        receivedMessageCount: '',
+        createdAt: '',
+        userId: localStorage.getItem('userId') || '',
+        accessToken: localStorage.getItem('accessToken') || '',
+        loginStatus: localStorage.getItem('loginStatus') || false,
+    },
+});
+
+axios.defaults.withCredentials = true;
+
+export const signUpUser = async (userInfo) => {
+    const res = await axios.post('http://ec2-3-35-22-107.ap-northeast-2.compute.amazonaws.com:8080/user', userInfo);
+    return res.data;
+};
+
+export const loginUser = async (userInfo) => {
+    const res = await axios.post('http://ec2-3-35-22-107.ap-northeast-2.compute.amazonaws.com:8080/login', userInfo);
+    const userId = res.headers.userid;
+    const accessToken = res.headers.authorization;
+    const loginStatus = true;
+    localStorage.setItem('userId', userId);
+    localStorage.setItem('accessToken', accessToken);
+    localStorage.setItem('loginStatus', loginStatus);
+    axios.defaults.headers.common['Authorization'] = `${accessToken}`;
+    return res;
+};
+
+export const authStateSelector = selector({
+    key: 'authStateSelector',
+    get: ({ get }) => {
+    const auth = get(authState);
+    return auth;
+    },
+    set: ({ set }, newAuth) => {
+    set(authState, { ...newAuth });
+    },
+});
+
+export const useAuthState = () => {
+    const [auth, setAuth] = useRecoilState(authStateSelector);
+    return [auth, setAuth];
+};
--- FE/front/src/pages/SB/MyBookShelf.js
+++ FE/front/src/pages/SB/MyBookShelf.js
@@ -7,19 +7,41 @@ import BSlist from '../../components/JSB/BookShelf/BSlist';
 import Pagenation from '../../components/common/Pagenation';
 import { useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 import bookData from '../../dummyData/SB/bookData'
+
+//1. 책을 하나씩 삭제할 수 있다.
+//2. 내가 등록한 책을 확인할 수 있다.
+//3. 내가 등록한 책을 누르면 해당 책 상세페이지로 이동할 수 있다.
+//4. 내가 등록한 책을 전체 삭제할 수 있다.
+//5. 책 상세페이지에서 상태가 변하면 내 책장에서 책이 삭제처리된다.
 
 const MyBookShelf = () => {
 	const cx = classNames.bind(style);
     const history = useNavigate();
-    const [book, setBook] = useState(bookData);
+    const [book, setBook] = useState([]);
 
     useEffect(()=>{
+        const fetchBooks = async () => {
+            try {
+                const response = await axios.get('http://ec2-3-35-22-107.ap-northeast-2.compute.amazonaws.com:8080/books');
+                const bookData = response.data;
+                const filteredData = bookData.filter((book) => book.exchanged !== '교환 완료')
+                setBook(bookData);
+                
+                console.log('Books Here', filteredData);
+            }catch(error){
+                console.error('error is Here', error);
+            }
+        };
+    
         const savedBooks = localStorage.getItem('book');
         if(savedBooks){
             setBook(JSON.parse(savedBooks));
         }
+        fetchBooks();
     }, []);
+
 
     const handleClick2 = (id) =>{
         const updatedList = book.filter(book => book.id !== id)
@@ -28,7 +50,7 @@ const MyBookShelf = () => {
         console.log(book)
         history('/myBookShelf')}
 
-    const handleClick3 = (id) =>{
+    const handleClick3 = () =>{
         const nullList = [];
         setBook(nullList);
         localStorage.setItem('book', JSON.stringify(nullList));
