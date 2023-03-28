5e6bed5fed1bd3afa81345d056f8fc5d012db26c
ASOpaper <hiji0907@gmail.com>
2023-03-28 11:42:06 +0900

    [feat]Router path 이름 변경

--- FE/front/src/App.js
+++ FE/front/src/App.js
@@ -39,15 +39,12 @@ function App() {
 					<Route path='/search' element={<BookSearchResult />}></Route>
 					<Route path='/search/detail/:isbn' element={<BookSearch />}></Route>
 					
-
-					
 					<Route path='/*' element={<ErrorPage />}></Route>
 					<Route path='/missing' element={<Missing />}></Route>
 					
-					
 					<Route element={<PrivateRoutes />}>
-					<Route path='/sellerDetailView' element={<SellerDetailView />}></Route>
-					<Route path='/sellerDetailView/edit' element={<SellerDetailViewEdit />}></Route>
+					<Route path='/seller/detailView' element={<SellerDetailView />}></Route>
+					<Route path='/seller/detailView/edit' element={<SellerDetailViewEdit />}></Route>
 
 					<Route path='/myPage' element={<MyPage />}></Route>
 					<Route path='/myPage/messageBox' element={<ReceivedMessages />}></Route>
@@ -61,8 +58,6 @@ function App() {
 					<Route path='/myPage/myRate' element={<UserRateList />}></Route>
 					<Route path='/myBookShelf' element={<MyBookShelf />}></Route>
 					</Route>
-
-
 
           	    </Routes>
 			</div>
