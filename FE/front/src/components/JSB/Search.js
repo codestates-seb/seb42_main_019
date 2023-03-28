36f89deb933ffd2f39357f9150bf3dc42bcc876c
ASOpaper <102563367+ASOpaper@users.noreply.github.com>
2023-03-28 11:50:34 +0900

    [bug] 충돌 수정

--- FE/front/src/components/JSB/Search.js
+++ FE/front/src/components/JSB/Search.js
@@ -1,31 +1,7 @@
 import { useNavigate } from 'react-router-dom';
 import style from './Search.module.css';
 
-<<<<<<< HEAD
-const HomeSearch = function ({ currentQuery, setCurrentQuery, searchBook }) {
-=======
 const HomeSearch = function () {
-
-  const params = useLocation();
-  const urlSearch = params.search;
-  const [currentQuery, setCurrentQuery] = useState(new URLSearchParams(urlSearch).get('q'));
-
-  const [bookData, setBookdata] = useRecoilState(bookSearchList);
-
-  const searchBook = async () => {
-      const url = `/books/search?keyword=${currentQuery}`;
-      try {
-          const res = await axios.get(url);
-          const books = res.data;
-          setBookdata(books);
-      } catch (error) {
-          console.log(error);
-      }
-  }
-  useEffect(() => {
-      setCurrentQuery(currentQuery);
-  }, [currentQuery, params])
->>>>>>> feat/FE
 
   const navigate = useNavigate()
 
