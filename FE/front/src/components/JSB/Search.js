b3880b82e7684da9ee8e3d30dddd820cc099b471
ASOpaper <hiji0907@gmail.com>
2023-03-28 23:14:08 +0900

    [bug]충돌

--- FE/front/src/components/JSB/Search.js
+++ FE/front/src/components/JSB/Search.js
@@ -2,7 +2,6 @@ import { useNavigate } from 'react-router-dom';
 import style from './Search.module.css';
 
 const HomeSearch = function () {
-
   const navigate = useNavigate()
 
 	const handleSearch = (event) => {
