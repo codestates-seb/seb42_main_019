61fd96367a945f1d625aea7f44a0bd3078d08018
journey0321 <yjyjh0321@gmail.com>
2023-04-01 21:17:45 +0900

    [bug]git 충돌해결

--- FE/front/src/api/api.js
+++ FE/front/src/api/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://ec2-15-165-131-229.ap-northeast-2.compute.amazonaws.com:8080'
-	// baseURL: 'http://localhost:8080',
-	,
+	baseURL: 'http://ec2-15-165-131-229.ap-northeast-2.compute.amazonaws.com:8080',
 	headers : {
 		Authorization: localStorage.getItem('accessToken')
 	}
